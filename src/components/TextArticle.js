521cebe566f235489c38df439d3b499289952e05
Unknown <Daniel.j.kafer@gmail.com>
2018-03-02 16:23:38 -0600

    Changed from Container to GridColumn

--- src/components/TextArticle.js
+++ src/components/TextArticle.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, GridColumn } from 'semantic-ui-react';
+import './../TextArticle.css';
 
 const TextArticle = (props) => (
-  <Container text>
-    <p>{props.content}</p>
-  </Container>
+  <GridColumn width="16" className="text-article-container">
+    <Container text>
+      <p className="text-article-content">{props.content}</p>
+    </Container>
+  </GridColumn>
 );
 
 export default TextArticle;
